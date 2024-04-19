f95c0ac6d98bbb2253dabe5c0367b81304959726
Sam Estrin <sam.estrin@gmail.com>
2024-04-19 13:35:06 -0700

    Update moduleConfig.js
    Added comments

--- src/functions/moduleConfig.js
+++ src/functions/moduleConfig.js
@@ -1,9 +1,21 @@
 // moduleConfig.js
 let config = {};
 
+/**
+ * Updates the global configuration object by merging the provided options.
+ *
+ * @param {object} options - An object containing configuration key-value pairs to add or override.
+ */
+
 function setup(options) {
   config = { ...config, ...options };
 }
+
+/**
+ * Returns a copy of the current global configuration object.
+ *
+ * @returns {object} A copy of the configuration object.
+ */
 
 function getConfig() {
   return config;
