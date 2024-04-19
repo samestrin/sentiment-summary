9db5c7cb1b29690b6c2badf03ba109d7a93633a5
Sam Estrin <sam.estrin@gmail.com>
2024-04-18 18:17:11 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const { sentimentTextRankSummary } = require("./textRank.js");
 const { sentimentLexRankSummary } = require("./lexRank.js");
-const { sentimentSeq2SeqSummary } = require("./sequenceToSequence.js");
+const { sentimentSeq2SeqSummary } = require("./sequenceToSequenceModels.js");
+const { sentimentExtractiveSummary } = require("./extractiveText.js");
+const { sentimentLSASummary } = require("./latentSemanticAnalysis.js");
 
 module.exports = {
   sentimentTextRankSummary,
   sentimentLexRankSummary,
   sentimentSeq2SeqSummary,
+  sentimentExtractiveSummary,
+  sentimentLSASummary,
 };
