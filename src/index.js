90645364cdd0cb8ff3ff5c555a99532d4ff46e6e
Sam Estrin <sam.estrin@gmail.com>
2024-05-08 09:51:11 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/**
+ * The sentiment-summary npm module generates text summaries weighted by the sentiment expressed in individual sentences.
+ * It analyzes the sentiment of each sentence within a larger body of text and emphasizes or deemphasizes sentences based
+ * on their sentiment scores during the summarization process.
+ *
+ * Copyright (c) 2024-PRESENT Sam Estrin
+ * This script is licensed under the MIT License (see LICENSE for details)
+ * GitHub: https://github.com/samestrin/sentiment-summary
+ */
+
 const { sentimentSummary } = require("./functions/summary.js");
 
 const {
